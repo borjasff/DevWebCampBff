d5140dbaee8d43df0c0ef17dc14bf97b684a20aa
borjasff <borjasff3@gmail.com>
2023-09-19 21:23:21 +0200

    subir router actualizado

--- Router.php
+++ Router.php
@@ -50,7 +50,7 @@ class Router
 
         //utilizar el layout segun la url
 
-        $url_actual = $_SERVER['PATH_INFO'] ?? '/';
+        $url_actual = strtok($_SERVER['REQUEST_URI'], '?') ?? '/';
 
         if(str_contains($url_actual, '/admin')){
             include_once __DIR__ . '/views/admin-layout.php';
